befcd9badbe86cfca945b0cc1f204e738d913841
Matthew Cline <clinemattw@gmail.com>
2019-07-03 20:22:48 -0500

    add styling to ApplicationRootScreen

--- components/ApplicationRootScreen.js
+++ components/ApplicationRootScreen.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Category from './CategoryScreen';
-import { AppRegistry, Image, View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import { AppRegistry, Image, ImageBackground, View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 class ApplicationRootScreen extends Component {
@@ -12,7 +12,7 @@ class ApplicationRootScreen extends Component {
 
   getCategories() {
     return (
-      <View style={{ flex: 1, backgroundColor: '#eaf1f8'}}>
+      <View style={styles.categoriesContainer}>
         <TouchableOpacity
           style={styles.categoryContainer}
           onPress={() => {
@@ -23,6 +23,10 @@ class ApplicationRootScreen extends Component {
             });
           }}
         >
+          <Image 
+            source={require('../assets/noun_person_1880095.png')} 
+            style={styles.categoryImage}
+          />
           <Text>
             People
           </Text>
@@ -97,6 +101,11 @@ class ApplicationRootScreen extends Component {
             Planets
           </Text>
         </TouchableOpacity>
+        <View 
+          style={{alignItems: 'center'}}
+        >
+          <Image style={{margin: 30}} source={require('../assets/ensigns.png')} />
+        </View>
 
       </View>
     );
@@ -104,13 +113,21 @@ class ApplicationRootScreen extends Component {
 
   getHeader() {
     return (
-      <Image source={require('../assets/application_root_header.png')} />
+      // <View style={{flex: 1, flexDirection: 'column', justifyContent: 'flex-start'}}>
+        <ImageBackground 
+          source={require('../assets/application_root_header.png')}
+          style={{flex: 1, width: '100%', height: '100%'}}
+        >
+          <Text>
+            Star wars
+          </Text>
+        </ImageBackground>
     );
   }
 
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         {this.getHeader()}
         {this.getCategories()}
       </View>
@@ -121,7 +138,8 @@ class ApplicationRootScreen extends Component {
 const styles = StyleSheet.create({
   categoryContainer: {
     flex: 1,
-    margin: 10,
+    flexDirection: 'row',
+    margin: 3,
     borderRadius: 4,
     borderWidth: 0.5,
     borderColor: '#d6d7da',
@@ -131,6 +149,15 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.9,
     shadowOffset: {width: 0, height: 5}
 
+  },
+  categoryImage: {
+    marginBottom: 2,
+    marginLeft: 5
+  },
+  categoriesContainer: {
+    flex: 2,
+    backgroundColor: '#eaf1f8', 
+    width: '100%'
   },
   title: {
     fontSize: 19,
